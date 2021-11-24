56d794136d5aee40a20437610f9d494a377a86dd
V4nkor <52006889+V4nkor@users.noreply.github.com>
2021-11-24 12:47:52 +0100

    Initialisation projet
    Ajout d'une page d'accueil avec controller et page twig + definission en tant que homepage

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
